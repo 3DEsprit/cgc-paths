6cc63c94d4faf87257aedd404f1dbfffd66dbb4a
DBrown <vilesyn@gmail.com>
2016-09-23 01:01:47 -0500

    react-router setup

--- App/index.js
+++ App/index.js
@@ -3,10 +3,18 @@ import './bootstrap/css/bootstrap.min.css'
 import './bootstrap/toastr.min.css'
 import './style.css'
 import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
 import { render } from 'react-dom';
 import App from './components/App';
+import List from './components/List';
 
 // Rendering main component
-render(<App />,
+render(<Router history={hashHistory}>
+  <Route path="/" component={App} />
+  <Route path="blender" handler={List} />
+  <Route path="concept" handler={List} />
+  <Route path="sculpt" handler={List} />
+  <Route path="unity" handler={List} />
+</Router>,
   document.getElementById('app')
 );
