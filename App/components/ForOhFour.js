795c1c91dc87e5917a844ae25dae1b34c9422014
DBrown <vilesyn@gmail.com>
2016-09-26 22:52:45 -0500

    404 page

---
+++ App/components/ForOhFour.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class ForOhFour extends Component {
+  render() {
+    return (
+      <div className="fourOh">
+        <h1>
+          Whoa! I don't think we're in Kansas anymore.
+        </h1>
+      </div>
+    )
+  }
+}
