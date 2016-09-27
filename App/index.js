8a70a7021ad76d8a890513eeb3816fea55b233bb
DBrown <vilesyn@gmail.com>
2016-09-27 13:41:01 -0500

    remove bootstrap/toastr

--- App/index.js
+++ App/index.js
@@ -1,6 +1,3 @@
-import './bootstrap/css/bootstrap-theme.min.css'
-import './bootstrap/css/bootstrap.min.css'
-import './bootstrap/toastr.min.css'
 import './styles.css'
 import React from 'react';
 import { render } from 'react-dom';
