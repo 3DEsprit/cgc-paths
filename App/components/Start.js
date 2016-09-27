3d847162d3a20a1bd4ed1a059863c2cc149d843f
DBrown <vilesyn@gmail.com>
2016-09-26 22:52:59 -0500

    Basic Front Page

---
+++ App/components/Start.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class List extends Component {
+  constructor(props, state) {
+    super(props, state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="start">Choose a Topic Above</h1>
+      </div>
+    )
+  }
+}
